9d488ddab4a175b29b383dd08b5cd8d317fb4481
Vasu-Patel-007 <patelvasu711@gmail.com>
2022-11-21 17:01:37 -0500

    coverted all the .mat files to csv to make development easy.

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import cv2
-
+import csv
 data_directory = "../cmpsc445_final_project/cars_train" # path to the training dataset folder
 image_size = 200 # image size to convert all the images into 
 training_dataset = [] # all the images will be converted to array and will be appended to this list
+
+# opening each csv files
+brand = open("../cmpsc445_final_project/brand.csv",'r')
+label = open("../cmpsc445_final_project/train_labels.csv",'r')
+
+csv_reader1 = csv.reader(brand)
+csv_reader2 = csv.reader(label)
+
+brand = list(csv_reader1)
+label = list(csv_reader2)
+
+print(brand[:11], ":",label[:11])
+
 
 for img in os.listdir(data_directory):
     # this for loop basically open each image in the folder and do the steps below and add it to training dataset[]
