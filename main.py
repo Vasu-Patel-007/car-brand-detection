62aaed7f3e3199d7f6b3bb739ba40efda3dc60b3
Vasu-Patel-007 <patelvasu711@gmail.com>
2022-11-21 14:55:16 -0500

    added comments to the code

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import matplotlib.pyplot as plt
 import os
 import cv2
 
-data_directory = "../cmpsc445_final_project/cars_train"
-image_size = 200
-training_dataset = []
+data_directory = "../cmpsc445_final_project/cars_train" # path to the training dataset folder
+image_size = 200 # image size to convert all the images into 
+training_dataset = [] # all the images will be converted to array and will be appended to this list
 
 for img in os.listdir(data_directory):
-    image_array = cv2.imread(os.path.join(data_directory, img), cv2.IMREAD_GRAYSCALE)
-    resized_image = cv2.resize(image_array,(image_size,image_size))
-    plt.imshow(resized_image,cmap="gray")
-    plt.show()
-    # print(image_array)
-    # print(image_array.shape)
+    # this for loop basically open each image in the folder and do the steps below and add it to training dataset[]
+    
+    image_array = cv2.imread(os.path.join(data_directory, img), cv2.IMREAD_GRAYSCALE) #read image and gray_scale it
+    resized_image = cv2.resize(image_array,(image_size,image_size)) # resize the image
+    # plt.imshow(resized_image,cmap="gray") # debugging purpose
+    # plt.show() # debugging purpose
+    # # print(image_array) # debugging purpose
+    # # print(image_array.shape) # debugging purpose
     break
     
 
