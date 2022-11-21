2910d89cd8debdd29d38a966c3752447de4d40e6
Vasu-Patel-007 <patelvasu711@gmail.com>
2022-11-21 14:52:00 -0500

    was able to load images and resize them. do grayscaling to them and conver them to an array.

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello world")
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+
+data_directory = "../cmpsc445_final_project/cars_train"
+image_size = 200
+training_dataset = []
+
+for img in os.listdir(data_directory):
+    image_array = cv2.imread(os.path.join(data_directory, img), cv2.IMREAD_GRAYSCALE)
+    resized_image = cv2.resize(image_array,(image_size,image_size))
+    plt.imshow(resized_image,cmap="gray")
+    plt.show()
+    # print(image_array)
+    # print(image_array.shape)
+    break
+    
+
+
